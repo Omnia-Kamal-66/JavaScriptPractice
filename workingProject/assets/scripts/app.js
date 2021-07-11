e154ce700c22e8612af2cd4059424e67b0609138
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-07-11 18:31:58 +0200

    4:17 is done

--- workingProject/assets/scripts/app.js
+++ workingProject/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10; //global value which is hardcoded so use this naming convention
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -8,16 +9,15 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode) {
-  let maxDamage;
-  if (mode === "ATTACK") {
-    maxDamage = ATTACK_VALUE;
-  } else if (mode === "STRONG_ATTACK") {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-  const damage = dealMonsterDamage(maxDamage); //this function will update the ui health bars
-  currentMonsterHealth -= damage;
-
+function endRound() {
+  /* this function is for monster attack only */
+  /*- the monster will hit us with ever button click ,
+     either we clicked on attack ,strong attack or heal button
+     - since the monster ill hit us ,even though we healed ourselves
+     we could theoretically be dead , so we need to check the win condition 
+     not the player because he dealt no damge when he pressed the heal button
+     
+     */
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
 
@@ -30,6 +30,21 @@ function attackMonster(mode) {
   }
 }
 
+function attackMonster(mode) {
+  /*  this function is for the player attack and at the end we call the function of the monster attack and to check win conditions*/
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  //action
+  const damage = dealMonsterDamage(maxDamage); //this function will update the ui health bars
+  currentMonsterHealth -= damage;
+  //reaction
+  endRound();
+}
+
 function attackHandler() {
   attackMonster("ATTACK");
 }
@@ -38,5 +53,22 @@ function strongAttackHandler() {
   attackMonster("STRONG_ATTACK");
 }
 
+function healPlayerHandler() {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("you can't heal to more than your max initial health");
+    healValue = chosenMaxLife - currentPlayerHealth; //we heal the player but not above inital max life
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  //action
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
+
+  //reaction
+  endRound();
+}
+
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
