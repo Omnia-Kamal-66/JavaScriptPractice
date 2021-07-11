0f5eda822a26d684dc975a8d128e9d663eb4b49e
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-07-11 18:01:31 +0200

    4:16 is done

--- workingProject/assets/scripts/app.js
+++ workingProject/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10; //global value which is hardcoded so use this naming convention
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,8 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE); //this function will update the ui health bars
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage); //this function will update the ui health bars
   currentMonsterHealth -= damage;
 
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -23,4 +30,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
