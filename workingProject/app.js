1f16d7e4654ad07a9338434dec818119a9b18062
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-07-19 23:24:27 +0200

    6:4 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,1 +1,12 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("Game is starting....");
+}
+const person = {
+  greet: function greet() {
+    console.log("Hello There!");
+  },
+};
+person.greet();
+startGameBtn.addEventListener("click", startGame);
