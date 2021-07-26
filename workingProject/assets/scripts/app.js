7197b938f2a9dac795fc7aa280f9620210c303b3
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-07-26 12:38:42 +0200

    7:31 is done

--- workingProject/assets/scripts/app.js
+++ workingProject/assets/scripts/app.js
@@ -1,9 +1,30 @@
-// const startAddMovieBtn = document.getElementsByTagName("header").children[1];
-const startAddMovieBtn = document.querySelector("header button");
-// const startAddMovieBtn = document.querySelector("header").lastElementChild;
-
 const addMovieModal = document.getElementById("add-modal");
 
-startAddMovieBtn.addEventListener("click", () => {
-  addMovieModal.className = "modal visible";
-});
+const startAddMovieBtn = document.querySelector("header button");
+
+const backdrop = document.getElementById("backdrop");
+
+// const cancelAddMovieBtn = document.querySelector("div button");
+
+const cancelAddMovieBtn = addMovieModal.querySelector(".btn--passive");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+startAddMovieBtn.addEventListener("click", toggleMovieModal);
+cancelAddMovieBtn.addEventListener("click", cancelAddMovie);
+backdrop.addEventListener("click", backdropClickHandler);
