73d7d6e9e63f7aa2569dbcdf47d5f68120238e76
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-01 00:19:34 +0200

    8:7 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,25 +1,9 @@
-const hobbies = ["sports", "cooking"];
-hobbies.push("reading"); //add a new element to the end of the array
-hobbies.unshift("coding"); //add a new element in the first place of the array
-const deletedElement = hobbies.pop(); //remove the item in the end
-hobbies.shift(); //remove the item at the beginnig of the array
-console.log(deletedElement);
-console.log(hobbies);
-
-//push and return also return something , a number which is the new length of the array
-//pop returns the element it removed
-//shift function , shifts all elements in the array one place to the left , which mean drops the first element
-//unshift , shifts all elements one place to the right
-//shift and unshift are slower than push and pop because they affect all elements
-
-hobbies[1] = "Coding";
-// hobbies[5] = "Reading"; //rarely used
-console.log(hobbies);
-
-//splice method , help us insert elements between two elements , used only on real arrays
-//splice returns the removed elements
-//we can use negative index , splice(-1 , 1) this will go to the end of the array
-hobbies.splice(1, 0, "Good Food"); //insert this elements in the first place you start looking
-console.log(hobbies);
-hobbies.splice(0, 1); //splice(0) deletes all array items , splice(1) skipps first element and delete the rest of the array and so on
-console.log(hobbies);
+const testResult = [1, 5.3, 1.5, 10.99, -5, 10];
+// const storedResult = testResult ; //if we changed one thing in testResult it will be reflected in stoedResults , because arrays are refrences to that place in memory
+//but if we ant to create a new array object , we can use slice(to copy arrays)
+/*slice is useful for selecting ranges of an array ,
+if you want to select two elements at the same time 
+*/
+// const storedResult = testResult.slice(0, 2); //selects 0 , 1 and 2 is not included
+const storedResult = testResult.slice(-3, -1);
+console.log(storedResult);
