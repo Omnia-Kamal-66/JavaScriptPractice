04100eeee5ff05ca68b4ba8b61b2dfca93bd05aa
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-07-31 22:55:52 +0200

    8:3 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,14 +1,22 @@
 const numbers = [1, 2, 3];
 console.log(numbers);
 
-// const moreNumbers = Array(5, 2);
+//niche , you won't need to use them
+
+// //constructor function
+// //caling array as a function
+// //if we pass one number in parameters , it will create an array of length 5 (empty array with fixed size)
+// const moreNumbers = new Array("hi", "welcome"); //you can pass as many parameters as you want , you can omit the new keyword
 // console.log(moreNumbers);
 
 // const yetMoreNumbers = Array.of(1, 2);
 // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+//it doesn't take multiple numbers , instead this takes an iterable or an array like object
+//it allows you to convert an iterable or an array like object which is not array yet to an array
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+const listItems = document.querySelectorAll("li");
+console.log(listItems); //this gives you a node list (an array like)
+
+const moreNumbers = Array.from(listItems);
+console.log(moreNumbers);
