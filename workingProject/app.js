f45c08fc180cda77873851c3f7459216dbf87955
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-01 00:26:37 +0200

    8:8 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -5,5 +5,13 @@ const testResult = [1, 5.3, 1.5, 10.99, -5, 10];
 if you want to select two elements at the same time 
 */
 // const storedResult = testResult.slice(0, 2); //selects 0 , 1 and 2 is not included
-const storedResult = testResult.slice(-3, -1);
+// const storedResult = testResult.slice(-3, -1);
+
+//adding elements to existing array and returning a brand new array
+/* concat method allows you to add elements to the end of an array 
+concat takes an array or multiple arrays but not individual items , 
+and combine these arrays with this array , so it doesn't add these arrays as new nested arrays
+
+*/
+const storedResult = testResult.concat([3.99, 2]);
 console.log(storedResult);
