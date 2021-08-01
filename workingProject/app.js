e540406551242b820c597cfd22537a119ba10664
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-02 01:31:20 +0200

    8:20 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,7 +1,19 @@
-const data = "new york;10.99;2000";
-const transformedData = data.split(";");
-console.log(transformedData);
+const persons = [
+  { name: "Omnia", age: 25 },
+  { name: "Ahmed", age: 29 },
+];
 
-const nameFragmants = ["Omnia", "Kamal"];
-const name = nameFragmants.join(" ");
-console.log(name);
+//this will be reflected in the copied persons array , because objects are refrence values
+// const copiedPersons = [...persons];
+
+//to avoid copying the refrence values of objects while using spread operator is to use map , and map returns an array so there is no nedd for the spread operator
+
+const copiedPersons = persons.map((person) => ({
+  name: person.name,
+  age: person.age,
+}));
+
+persons.push({ name: "Aya", age: 27 });
+persons[0].age = 26;
+
+console.log(copiedPersons);
