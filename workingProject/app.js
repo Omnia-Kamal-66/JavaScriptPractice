37b0fedff9a759356009a372f052d8a80ea19155
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-02 00:35:30 +0200

    8:13 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,7 +1,7 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const taxAdjustedPrices = [];
+// const taxAdjustedPrices = [];
 // for (const price of prices) {
 //   taxAdjustedPrices.push(price * (1 + tax));
 // }
@@ -13,10 +13,14 @@ the value it's currently looking at ,
 and the index of the element
 and the full array
 
+-map method : 
+has a job of taking an array , running a function , which has this form on every item in that array
+and that function should return a new element for every element in that array
+
 */
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = { index: idx, taxAdjustedPrice: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
