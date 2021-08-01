d186032323d632ceb7c0d3111f37551b2db8ba74
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-01 23:58:47 +0200

    8:11 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,4 +1,6 @@
 const testResult = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+
+console.log(testResult.includes(10.99));
 // const storedResult = testResult ; //if we changed one thing in testResult it will be reflected in stoedResults , because arrays are refrences to that place in memory
 //but if we ant to create a new array object , we can use slice(to copy arrays)
 /*slice is useful for selecting ranges of an array ,
