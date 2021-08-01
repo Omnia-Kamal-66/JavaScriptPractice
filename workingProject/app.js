9dcd2898fea8df441a3f7df97851809475d63ef0
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-02 01:38:52 +0200

    8:21 is done

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,19 +1,11 @@
-const persons = [
-  { name: "Omnia", age: 25 },
-  { name: "Ahmed", age: 29 },
-];
+//array destructuring
+const nameData = ["Omnia", "Kamal", "Mrs", 25];
+//instead of
+// const fName= nameData[0];
+// const lName = nameData[1];
+//we do
 
-//this will be reflected in the copied persons array , because objects are refrence values
-// const copiedPersons = [...persons];
-
-//to avoid copying the refrence values of objects while using spread operator is to use map , and map returns an array so there is no nedd for the spread operator
-
-const copiedPersons = persons.map((person) => ({
-  name: person.name,
-  age: person.age,
-}));
-
-persons.push({ name: "Aya", age: 27 });
-persons[0].age = 26;
-
-console.log(copiedPersons);
+const [fName, lName, ...otherInfo] = nameData;
+console.log(fName);
+console.log(lName);
+console.log(otherInfo);
