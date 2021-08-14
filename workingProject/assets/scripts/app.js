45d9c006cadb82b694c149058a6360880ccdc9a4
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-14 15:49:31 +0200

    10:3 is done

---
+++ workingProject/assets/scripts/app.js
@@ -0,0 +1,45 @@
+/* This a very simple dummy shop, a list of products where we can click an 
+Add to Cart button which will then increment the total amount in a shopping cart
+*/
+
+const productList = {
+  products: [
+    {
+      title: "A pillow",
+      imageUrl: "",
+      price: 19.99,
+      decription: "A soft pillow",
+    },
+    {
+      title: "A carpet",
+      imageUrl: "",
+      price: 89.99,
+      decription: "A carpet which you might like or not",
+    },
+  ],
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+            <div>
+                <img src="${prod.imageUrl}" alt="${prod.title}"> 
+            </div>
+            <div class='product-item__content'>
+              <h2>${prod.title}</h2>
+              <h3>${prod.price}</h3>
+              <p>${prod.decription}</p>
+              <button>Add to Cart</button>            
+            </div>
+            `;
+      prodList.append(prodEl);
+    }
+
+    renderHook.append(prodList);
+  },
+};
+
+productList.render();
