37335db5020c56065c8ee519c7c24f5fbd538f0c
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-16 14:42:02 +0200

    14:4 is done

--- workingProject/assets/scripts/events.js
+++ workingProject/assets/scripts/events.js
@@ -2,15 +2,20 @@ const button = document.querySelector("button");
 // button.onclick= function(){
 
 // };
-// const ButtonClickHandler = () => {
-//   alert("Button was clicked");
-// };
-// const anotherButtonClickHandler = () => {
-//   console.log("This was clicked!");
-// };
+const ButtonClickHandler = () => {
+  alert("Button was clicked");
+};
+const anotherButtonClickHandler = () => {
+  console.log("This was clicked!");
+};
 // button.onclick = ButtonClickHandler; //we can only add one handler to this event on this element
 // button.onclick = anotherButtonClickHandler;//this is the only function that will excute
 
 //this approach allows you to add more than one function as an event handler
-button.addEventListener();
-button.removeEventListener();
+
+const boundFn = ButtonClickHandler.bind(this);
+button.addEventListener("click", boundFn);
+
+setTimeout(() => {
+  button.removeEventListener("click", boundFn);
+}, 2000);
