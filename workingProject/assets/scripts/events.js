46e113e3d41be742193d1cc0fd85c0732e4c7ac6
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-16 15:40:30 +0200

    14:8 is done

--- workingProject/assets/scripts/events.js
+++ workingProject/assets/scripts/events.js
@@ -2,29 +2,35 @@ const buttons = document.querySelectorAll("button");
 // button.onclick= function(){
 
 // };
-const ButtonClickHandler = (event) => {
-  //   event.target.disabled = true;
-  console.log(event);
-};
-const anotherButtonClickHandler = () => {
-  console.log("This was clicked!");
-};
+// const ButtonClickHandler = (event) => {
+//   //   event.target.disabled = true;
+//   console.log(event);
+// };
+// const anotherButtonClickHandler = () => {
+//   console.log("This was clicked!");
+// };
 // button.onclick = ButtonClickHandler; //we can only add one handler to this event on this element
 // button.onclick = anotherButtonClickHandler;//this is the only function that will excute
 
 //this approach allows you to add more than one function as an event handler
 
-const boundFn = ButtonClickHandler.bind(this);
+// const boundFn = ButtonClickHandler.bind(this);
 // button.addEventListener("click", ButtonClickHandler);
 
 // setTimeout(() => {
 //   button.removeEventListener("click", ButtonClickHandler);
 // }, 2000);
 
-buttons.forEach((btn) => {
-  btn.addEventListener("mouseenter", ButtonClickHandler);
-});
+// buttons.forEach((btn) => {
+//   btn.addEventListener("mouseenter", ButtonClickHandler);
+// });
 
-window.addEventListener("scroll", (event) => {
+// window.addEventListener("scroll", (event) => {
+//   console.log(event);
+// });
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
   console.log(event);
 });
