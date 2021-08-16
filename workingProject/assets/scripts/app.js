66e5aa06bb1a7b84b655418bd99ffbe3edffbf1d
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-16 07:52:08 +0200

    12:6 is added

--- workingProject/assets/scripts/app.js
+++ workingProject/assets/scripts/app.js
@@ -16,22 +16,47 @@ given the data which might be coming from a server , we just wnt to be able to m
 
 */
 
+class DOMHelper {
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
+
+  static moveElement(elementId, newDestinationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    destinationElement.append(element);
+  }
+}
+
 class ToolTip {}
 
 class ProjectItem {
-  constructor(id, updateProjectsListsFunction) {
+  constructor(id, updateProjectsListsFunction, type) {
     this.id = id;
     this.updateProjectsListsHandler = updateProjectsListsFunction;
-    this.connectSwitchButton();
+    this.connectSwitchButton(type);
     this.connectMoreInfoButton();
   }
   connectMoreInfoButton() {}
 
-  connectSwitchButton() {
+  connectSwitchButton(type) {
     const projItemEl = document.getElementById(this.id);
-    const switchBtn = projItemEl.querySelector("button:last-of-type");
+    let switchBtn = projItemEl.querySelector("button:last-of-type");
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === "active" ? "finished" : "Activate";
+
     //when we click the btn we want to remove this projItem from its list and add it to the other project list
-    switchBtn.addEventListener("click", this.updateProjectsListsHandler);
+    switchBtn.addEventListener(
+      "click",
+      this.updateProjectsListsHandler.bind(null, this.id)
+    );
+  }
+
+  update(updateProjectsListsFunction, type) {
+    this.updateProjectsListsHandler = updateProjectsListsFunction;
+    this.connectSwitchButton(type);
   }
 }
 
@@ -43,7 +68,7 @@ class ProjectList {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
       this.projects.push(
-        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
       );
     }
     console.log(this.projects);
@@ -52,12 +77,20 @@ class ProjectList {
   setSwitchHandlerFunction(switchHandlerFunction) {
     this.switchHandler = switchHandlerFunction;
   }
+  //what we need to do: make sure whrn we switch a project ,we also update that project to change the button caption and add a new event listener
 
-  addProject() {
-    console.log(this);
+  addProject(project) {
+    this.projects.push(project); //now we moved the proj from a list to another in js
+
+    console.log(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    //we should pass a switch project function , because when we add a project,that means we switch the lists,it switch from instance A to instance B ,
+    //and therefor this function(switch Handler) to wich this project is bound in the end has to be changed because that still points at the old instance A , the project is now an instance B ,so we have to pass the switchproject as it is defined in instance B
+    project.update(this.switchProject.bind(this), this.type);
   }
 
   switchProject(projectId) {
+    console.log(projectId);
     this.switchHandler(this.projects.find((p) => p.id === projectId)); //we 're passing the project tjo thw switch handler function
     this.projects = this.projects.filter((p) => p.id !== projectId);
   }
