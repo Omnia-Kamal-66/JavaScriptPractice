f9f2950f32be5751536e7a4a2acf32a3d7ad2a3a
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-17 23:08:57 +0200

    18:9 is done

--- workingProject/assets/scripts/app.js
+++ workingProject/assets/scripts/app.js
@@ -1,24 +1,39 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest(); //aloows you to send http request ,it is built in the browser
+function sendHttpRequests(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest(); //aloows you to send http request ,it is built in the browser
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts"); //
+    xhr.open(method, url); //
 
-xhr.responseType = "json"; //instead of json.parse
+    xhr.responseType = "json"; //instead of json.parse
 
-xhr.onload = function () {
-  const listOfPosts = xhr.response;
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    xhr.send(); //this will send a request
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequests(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
     listElement.append(postEl);
   }
-};
-
-xhr.send(); //this will send a request
-
+}
+fetchPosts();
 /*
 JSON constructor function has a couple of static helper methods,
 -parse :helps to convert json to javascript
