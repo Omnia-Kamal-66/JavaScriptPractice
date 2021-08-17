7e0d2105f0eca1cd415e4ea9d6a698f6e28623a6
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-17 19:09:43 +0200

    17:9  code is added

--- workingProject/app.js
+++ workingProject/app.js
@@ -1,33 +1,61 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+/*
 
-const setTimer = duration => {
+
+*/
+
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+const setTimer = (duration) => {
+  //promise takes a function as an argument , which will be excuted when the promise is constructed
+  //this function takes to arguments , and each argument is also a function
+  //now the timer is set when the promise is created
+  //after creating the promise , not after the timer is done , i will return the promise so i can use it where ever i call the set timer function
+  //i will call the resolve in set time out , so the browser will send it after the promise is created
+  //resolve function will mark this promise object as done
+  //setTimer does return a promise , you can call then on it , you can call this then method when a promise is resolved ,so when the timer is done
+
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('Done!');
+      resolve("Done!");
     }, duration);
   });
+
+  return promise;
+};
+
+const getPosition = () => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {}
+    );
+  });
+
   return promise;
 };
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimer(2000).then(data => {
-        console.log(data, posData);
-      });
-    },
-    error => {
-      console.log(error);
-    }
-  );
+  let positionData;
+
+  getPosition()
+    .then((posData) => {
+      positionData = posData;
+      //here the promise is resolved , but when we return something in the then callback function , this will set the promise to pending , and we will wait to the returned promise to be resolved ,and now we can add a new then block after the first one
+      return setTimer(2000); //the promise of the getposittion is now set back from being done to being pending
+    })
+    .then((data) => {
+      console.log(data, positionData);
+    }); //here i get the data of that inner promise,which in this case the timer data
   setTimer(1000).then(() => {
-    console.log('Timer done!');
+    console.log("Timer Done");
   });
-  console.log('Getting position...');
+  console.log("Getting position...");
 }
 
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
 
 // let result = 0;
 
