41a792dc6d65d2ba3175878f051a6b57945440c7
Omnia-Kamal-66 <omniakamal661996@gmail.com>
2021-08-18 17:21:16 +0200

    Module 20 is done

--- workingProject/app.js
+++
@@ -1,5 +0,0 @@
-const customers = ["Max", "Manuel", "Anna"];
-const activeCutomers = ["Max", "Manuel"];
-
-const inactiveCustomers = _.difference(customers, activeCutomers);
-console.log(inactiveCustomers);
